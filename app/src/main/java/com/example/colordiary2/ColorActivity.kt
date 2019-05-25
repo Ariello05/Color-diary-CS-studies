7f242da7931461828a05f021f49562a1624ebc46
Ariello05 <heriu@wp.pl>
2019-05-25 04:18:21 +0200

    + Alpha version of colorActivity:  + Loading presets names  + Defining default presets on empty

---
+++ app/src/main/java/com/example/colordiary2/ColorActivity.kt
@@ -0,0 +1,54 @@
+package com.example.colordiary2
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_color.*
+import android.R.layout
+import android.graphics.Color
+import android.widget.Spinner
+import android.widget.ArrayAdapter
+
+
+
+class ColorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color)
+
+        val mu :HashSet<String>
+        val sp = getSharedPreferences("MainResource", Context.MODE_PRIVATE)
+        mu = sp.getStringSet(getString(R.string.colorPresets),HashSet<String>()) as HashSet<String>//Get list of known colorSets
+
+        val adapter = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item, mu.toList()
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerPresets.adapter = adapter
+    }
+
+    fun getNameOfSelectedPreset(): String?{
+        val sp = getSharedPreferences("MainResource", Context.MODE_PRIVATE)
+        return sp.getString(getString(R.string.currentColorSet),"")//Loads name of main color set in program
+    }
+
+}
+
+/*
+currentSet = ColorSet(applicationContext)
+val str = getNameOfCurrentPreset()
+if(str == ""){
+    currentSet.insert(Color.parseColor("#000000"), "Sleep")
+    currentSet.insert(Color.parseColor("#FFFFFF"), "Living")
+}else {
+    currentSet.loadSet(str!!)
+}
+
+val adapter = ArrayAdapter<String>(
+    this, android.R.layout.simple_spinner_item, currentSet.getActivityNameArrayList()
+)
+
+adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+spinnerPresets.adapter = adapter
+*/
